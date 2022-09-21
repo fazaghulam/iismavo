d78fc802c917eb6a900c8481e722d616447a24ce
Faza Ghulam Ahmad <fazaghulam@gmail.com>
2022-09-21 23:31:57 +0100

    style: add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        primary: "#112233",
+      },
+    },
   },
   plugins: [],
 };
